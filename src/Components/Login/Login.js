6f029cd85c0af07a72a7d147aa57430f831279f1
denismahmutovic <djdeny95@gmail.com>
2022-10-27 19:33:27 +0200

    login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,6 +3,21 @@ import axios from "axios";
 import { useContext, useState } from "react";
 import { TokenContext } from "../Context/UserContext";
 import { useNavigate } from "react-router-dom";
+import LoginCss from "./Login.css";
+
+import Avatar from "@mui/material/Avatar";
+
+import CssBaseline from "@mui/material/CssBaseline";
+
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const LoginPage = () => {
   const [user, setUser] = useState("");
@@ -33,35 +48,93 @@ const LoginPage = () => {
       });
   };
 
+  // return (
+  //   <>
+  //     <form onSubmit={submitHandler}>
+  //       <div>
+  //         <h2>Login</h2>
+  //         <div>
+  //           <label htmlFor="name">Name: </label>
+  //           <input
+  //             type="text"
+  //             name="name "
+  //             id="name"
+  //             onChange={(e) => setUser(e.target.value)}
+  //           ></input>
+  //         </div>
+  //         <div>
+  //           <label htmlFor="password">Password:</label>
+  //           <input
+  //             type="password"
+  //             name="password"
+  //             id="password"
+  //             onChange={(e) => setPassword(e.target.value)}
+  //           ></input>
+  //         </div>
+  //         <div>
+  //           <input type="submit" value="LOGIN"></input>
+  //         </div>
+  //       </div>
+  //     </form>
+  //   </>
+  // );
+  const theme = createTheme();
+
   return (
-    <>
-      <form onSubmit={submitHandler}>
-        <div>
-          <h2>Login</h2>
-          <div>
-            <label htmlFor="name">Name: </label>
-            <input
-              type="text"
-              name="name "
-              id="name"
+    <ThemeProvider classname="color" theme={theme}>
+      <Container component="main" maxWidth="xs" onSubmit={submitHandler}>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="User Name"
+              name="Name"
               onChange={(e) => setUser(e.target.value)}
-            ></input>
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
               name="password"
-              id="password"
+              label="Password"
+              type="password"
               onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </div>
-          <div>
-            <input type="submit" value="LOGIN"></input>
-          </div>
-        </div>
-      </form>
-    </>
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 };
 
--- src/Components/Quotes/Quotes.js
+++ src/Components/Quotes/Quotes.js
@@ -24,15 +24,15 @@ export default function Quotes() {
   return (
     <div className="quoteListContainer">
       <h1>Quotes</h1>
+
       {quoteArray.map((el) => {
         return (
           <div className="dva" key={el.id}>
-            <div>
+            <div className="flex">
               <p className="content">
-                {" "}
-                {el.content} {el.author}{" "}
+                {el.content} {el.author}
               </p>
-              <p className=""> {el.author} </p>
+              <p className="flex-dva"> {el.author} </p>
             </div>
           </div>
         );
